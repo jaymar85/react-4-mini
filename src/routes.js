4f28d8a1f023e5ed94bcc35671378c1b5cc933d5
jaymar85 <jermark851@gmail.com>
2021-06-18 00:17:51 -0500

    complete

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import Home from "../src/Components/Home/Home"
-import TopicList from "../src/Components/TopicList/TopicList"
-import Post from "../src/Components/Post/Post"
+import Home from "./Components/Home/Home"
+import TopicList from "./Components/TopicList/TopicList"
+import Post from "./Components/Post/Post"
 
 export default (
     <Switch>
